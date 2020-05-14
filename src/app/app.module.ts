f667288f8931a6ce9cac27d067d7f4349d7f0896
Kyriakos Karagkiozidis <kirk.karagiozidis@gmail.com>
2020-05-14 16:28:00 +0300

    End of lecture 93.
    End of Section 7.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
 // Routing
 import { AppRoutingModule } from './app-routing.module';
 // Components
@@ -36,6 +37,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatToolbarModule,
     MatExpansionModule,
     MatProgressSpinnerModule,
+    MatPaginatorModule,
     HttpClientModule
   ],
   providers: [],
