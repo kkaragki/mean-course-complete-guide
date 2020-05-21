9f543ba1081bcd6caa25603692ced8268fb9bb2b
Kyriakos Karagkiozidis <kirk.karagiozidis@gmail.com>
2020-05-21 14:38:46 +0300

    End of lecture 135.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-// Angular Material
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog';
 // Routing
 import { AppRoutingModule } from './app-routing.module';
+// Angular Material Module
+import { AngularMaterialModule } from './angular-material.module';
+// Auth Module
+import { AuthModule } from './auth/auth.module';
+// Posts Module
+import { PostsModule } from './posts/posts.module';
 // Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignUpComponent } from './auth/signup/signup.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
 import { ErrorComponent } from './error/error.component';
 // Interceptors
 import { AuthInterceptor } from './auth/auth-interceptor';
@@ -30,27 +23,16 @@ import { ErrorInterceptor } from './error-interceptor';
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
-    SignUpComponent,
-    PostCreateComponent,
-    PostListComponent,
     ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
     HttpClientModule,
-    MatDialogModule
+    AngularMaterialModule,
+    AuthModule,
+    PostsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
